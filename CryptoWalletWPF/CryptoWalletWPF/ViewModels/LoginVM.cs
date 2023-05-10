cd5156af13e4a510b4b15823bf640ddf50783fe0
Netryle <61221809+Netryle@users.noreply.github.com>
2023-05-10 13:01:26 +0700

    [ADD] Add network combobox content
    Add network combobox content in Login view model

---
+++ CryptoWalletWPF/CryptoWalletWPF/ViewModels/LoginVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoWalletWPF.ViewModels
+{
+    class LoginVM : INotifyPropertyChanged
+    {
+        private List<string> networkList 
+        {
+            get 
+            {
+                return this.networkList;
+            }
+            set
+            {
+                this.networkList = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
